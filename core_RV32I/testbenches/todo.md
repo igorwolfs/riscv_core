57fc2b0e4c5b75bad30bb449c4d6ee505a2b91c5
igorwolfs <wolfs.igor@hotmail.com>
2025-02-23 16:55:04 +0100

    modified testbench to be able to read/write from external memory

--- core_RV32I/testbenches/todo.md
+++ core_RV32I/testbenches/todo.md
@@ -1,5 +1,5 @@
 # Todo
-## Step 0
+## Correct yaml file, linker script and header file for assembly generation
 - Put the correct yaml file in order for the simplest architecture.
 - Try the yaml-check command:
 ### Write the Python plugin for initialization and compilation
@@ -14,13 +14,69 @@ Error: config: <Section: spike_simple>
 So a Section class needs to be added. 
 - Something that is somehow done for the other modules but not for this one?
 - Where does the "config" come from? Which file is it generated from?
-## Step 1: compile a single raw test using the 32-bit riscv compiler and an assembly example
 
+## Test folder
+Run command:
 
-## Step 2: Load the test into memory (instruction memory and data memory)
+```bash
+riscof testlist --config=config.ini --suite=riscv-arch-test/riscv-test-suite/ --env=riscv-arch-test/riscv-test-suite/env
+```
 
+Generates a list of folders for tests.
 
-## Step 3: Compare the output signatures to the reference output signature
+## Test compilation
+
+- Enable command for compiling tests .elfs and dumping them into */test_name/dut/-folder
+
+### Setting up the simulation environment / verilating the model
+
+- The run.sh now sets up the simulation environment
+- The elfs and signature file-paths can be passed to it during execution.
+
+### Compiling the test-elfs
+
+- Test-elfs were compiled.
+
+### Loading test-elfs into memory
+Now we can load the test-elfs into memory.
+I would expect this to be as simple as
+- call the binary with
+  - path-to-memory
+  - path-to-signature
+
+Then the binary should output the signature to the relevant file
+
+### Making the risc5 cpu able to read / write
+- Inside the testbench
+  - Load the file into memory.
+  - It should be kind-of in "RAM", so it can be read from and written to as if it were in RAM.
+  - Then there should be a second always-loop which checks for the write address / memory /don't know?
+    - Depending on this address it should send something to the signature file.
+
+- Take into account the special registers at addresses like 0xFF000000
+- It seems that inside the assembly everything is written by byte.
+- In my registers however everythin is a 32-bit word.
+  - Address passed with instructions is 32-bit address.
+  - It uses byte addressing
+  - SO: if the instructions use byte-addressing 
+    => We need to skip these first 2 bit-indices.
+
+Seems like I have a prototype which does compile
+
+### Compiling the simulation and running it with the relevant files / signature write-to file.
+We need to create a bash-script that can
+- Compile the project with verilator to a location (e.g.: sim/)
+- Run the binary
+
+Done
+
+### Compiling the tests
+- Works, for example: dut/my.elf
+- Disasembled
+
+## Signature generation
+
+## Signature comparison with reference
 
 It seems like in the neorv32 part of this happens by a macro defined in the model_test.h file:
 ```C
--- core_RV32I/testbenches/top_tb.sv
+++ core_RV32I/testbenches/top_tb.sv
@@ -1,9 +1,31 @@
-module top_tb #(parameter mem_content_path="tests/my.hex",
-                parameter signature_path = "tests/my.sig")
-    ();
+`timescale 1ns/10ps
 
+// 1 MB: 0b1111_11111111_11111111 = 20 bits = 0xFFFFF
+
+module top_tb #(parameter INTERNAL_MEMORY=1'b0, parameter MEMSIZE=4*1024*1024) (); // 4 MB (in bytes)
     reg sysclk = 0, nrst_in = 1;
-    // External memory interface (when INTERNAL_MEMORY = 0)
+    integer sig_file;
+
+    // **** External memory interface (when INTERNAL_MEMORY = 0) ****
+    // *** FILE HANDLING
+    reg [31:0] ext_memory [0:MEMSIZE-1]; // 16 MB of memory
+    string mem_path, sig_path;
+    initial begin
+        if (!$value$plusargs("MEM_PATH=%s", mem_path)) mem_path = "tests/my.hex";
+        if (!$value$plusargs("SIG_PATH=%s", sig_path)) sig_path = "tests/my.sig";
+
+        // Load memory file
+        $readmemh(mem_path, ext_memory);
+
+        // Open signature file
+        sig_file = $fopen(sig_path, "w");
+        if (sig_file == 0) begin
+            $display("Error: Could not open signature file %s", sig_path);
+            $finish;
+        end
+    end
+
+    // *** MEMORY INTERFACING
     // Data Memory Interface
     wire [31:0] dmem_rd_addr, dmem_rd_data, dmem_wr_addr, dmem_wr_data;
     wire dmem_wr_en;
@@ -11,7 +33,39 @@ module top_tb #(parameter mem_content_path="tests/my.hex",
     // Instruction Memory Interface
     wire [31:0] imem_addr, imem_data;
 
-    core #(.INTERNAL_MEMORY(0)) core_t (.sysclk(sysclk), .nrst_in(nrst_in),
+
+    // Memory read logic  (Making sure any special address space is ignored)
+    assign dmem_rd_data = ext_memory[dmem_rd_addr[23:2]];
+    assign imem_data = ext_memory[imem_addr[23:2]];  // Instruction fetch
+
+
+    // Memory write logic
+    always @(posedge sysclk) begin
+        if (dmem_wr_en)
+            begin
+            if (dmem_wr_addr[31:28] == 4'h0) begin
+                // Normal memory write
+                ext_memory[dmem_wr_addr[23:2]] <= dmem_wr_data;
+            end
+            else if (dmem_wr_addr == 32'hF0000004) // Address for signature write
+                begin
+                ext_memory[dmem_wr_addr[23:2]] <= dmem_wr_data;
+                // Example: Write to signature file on memory writes
+                $fdisplay(sig_file, "Write to addr %h: data %h\r\n", dmem_wr_addr, dmem_wr_data);
+                end
+            else if ((dmem_wr_addr == 32'hCAFECAFE) && (dmem_wr_data == 32'hF0000000))
+                begin
+                $display("Finishing simulation.");
+                $fclose(sig_file);
+                $finish;
+                end
+            else;
+            end
+        else;
+    end
+
+    // **** CORE ****
+    core #(.INTERNAL_MEMORY(INTERNAL_MEMORY)) core_t (.sysclk(sysclk), .nrst_in(nrst_in),
     // Data
     .dmem_rd_addr(dmem_rd_addr), .dmem_rd_data(dmem_rd_data), .dmem_wr_addr(dmem_wr_addr),
     .dmem_wr_data(dmem_wr_data), .dmem_wr_en(dmem_wr_en),
@@ -25,14 +79,4 @@ module top_tb #(parameter mem_content_path="tests/my.hex",
         #10;
         nrst_in = 1;
     end
-
 endmodule
-
-/**
-IDEA 1:
-- Create a function that on "read" reads memory from the hex file and loads it.
-- Then set the reset vector as well as the instruction pointer appropriately to start.
-- Then run the code
-- While running check for memory values indicating signature region
-- Write the signature values to a separate file
-*/
